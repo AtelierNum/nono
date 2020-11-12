70ff8fd0478f403478a20574baab3f2c60762319
r4ph3rd <hav4na@protonmail.com>
2020-11-12 14:35:32 +0100

    remove stateful for led

--- commands/arduino/led.js
+++ commands/arduino/led.js
@@ -11,13 +11,13 @@ exports.builder = {
 };
 
 exports.handler = async function (argv) {
-  if (argv.state == 'state'){
+  /* if (argv.state == 'state'){
     console.log(`Sur une échelle de 1 à 12, de 1 à 4 la led est éteinte. 
     De 4 à 7 elle est allumée. C'est seulement de 8 à 9 qu'elle est psychologiquement en train de s'allumer. 
     Mais de 9 à 11, elle est plutôt en train de s'éteindre ! Et ce n'est qu'à 12 qu'elle est ni éteinte ni allumée !`)
-  } else {
+  } else { */
     
     const led = new arduino.five.Led(13);
     argv.state == 'on' ? led.on() : led.off();
-  }
+  // }
 };
