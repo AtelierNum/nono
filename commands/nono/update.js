431459c38b4d8bafa6a1f9d7cefbfbb8e01d05d5
valentin dupas <valentin.lc.dupas@gmail.com>
2021-05-26 07:53:35 +0200

    install deps after self-pulling

--- commands/nono/update.js
+++ commands/nono/update.js
@@ -1,12 +1,14 @@
 const pm2 = require("pm2");
 const git = require("isomorphic-git");
 const http = require("isomorphic-git/http/node");
+const ps = require("child_process");
 const fs = require("fs");
 const appRoot = require("app-root-path").toString();
 
 exports.describe = "attempts to upgrade by pulling from the git repo";
 
 exports.handler = async argv => {
+	argv.msg.channel.send("attempting auto update");
 	try {
 		await git.pull({
 			fs,
@@ -18,6 +20,11 @@ exports.handler = async argv => {
 				name: "nono",
 			},
 		});
+		const yarnMsg = ps.execSync("yarn");
+		if(!yarnMsg.toLowerCase().includes("success")){
+			throw yarnMsg;
+		}
+		
 		argv.msg.channel.send("pull complete, restarting...");
 		await pm2.connect();
 		pm2.restart("nono");
